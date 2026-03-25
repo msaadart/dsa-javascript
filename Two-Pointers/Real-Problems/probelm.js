c01abc99dcc6128fb2aaa97a757385861f5865d7
MuhammaFarjad Khan <farjadkhan823@gmail.com>
2026-03-25 15:49:37 +0500

    JS-DSA

--- Two-Pointers/Real-Problems/probelm.js
+++ Two-Pointers/Real-Problems/probelm.js
@@ -1,27 +1,25 @@
 // Problems //
 
 // 1 - Squaring a Sorted Array
-
-let arr = [-4, -2, 7, 8, 11];
+let arr = [-2, -4, 9, -1, 8, 12];
+arr.sort((a, b) => a - b)
 
 let left = 0;
 let right = arr.length - 1;
-let result = new Array(arr.length);
+let res = new Array(arr.length);
 let i = arr.length - 1;
 
 while (left <= right) {
-
     let leftSquare = arr[left] * arr[left];
     let rightSquare = arr[right] * arr[right];
 
     if (leftSquare > rightSquare) {
-        result[i] = leftSquare;
-        left++;
+        res[i] = leftSquare;
+        left++
     } else {
-        result[i] = rightSquare;
-        right--;
+        res[i] = rightSquare
+        right--
     }
-
     i--;
 }
-console.log(result);
+console.log(res)
