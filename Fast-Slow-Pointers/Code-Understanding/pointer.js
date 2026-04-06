587ad559afee0ff484e44cc46d5f9e22c7d170b9
Muhammad Farjad Khan <farjadkhan823@gmail.com>
2026-04-06 11:53:37 +0500

    JS-DSA

--- Fast-Slow-Pointers/Code-Understanding/pointer.js
+++ Fast-Slow-Pointers/Code-Understanding/pointer.js
@@ -9,14 +9,29 @@ let node1 = new Node(1);
 let node2 = new Node(2);
 let node3 = new Node(3);
 let node4 = new Node(4);
+let node5 = new Node(5);
 
 node1.next = node2;
 node2.next = node3;
 node3.next = node4;
+node4.next = node5;
+node5.next = node3;
 
-let current = node1;
+function detectCycle(head) {
+    let slow = head;
+    let fast = head;
 
-while (current !== null) {
-    console.log(current.data);
-    current = current.next;
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            console.log("Cycle detected at node:", slow.data);
+            return slow;
+        }
+    }
+
+    return null;
 }
+
+let meetingNode = detectCycle(node1);
