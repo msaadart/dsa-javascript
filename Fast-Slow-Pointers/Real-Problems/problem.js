1adea7eae08f50720ab3f5545744213554d6bbbc
Muhammad Farjad Khan <farjadkhan823@gmail.com>
2026-04-07 15:23:55 +0500

    JS-DSA

--- Fast-Slow-Pointers/Real-Problems/problem.js
+++ Fast-Slow-Pointers/Real-Problems/problem.js
@@ -1,39 +1,38 @@
  //// 1 - middle of the linkedlist
 
- //  class Node {
- //      constructor(data) {
- //          this.data = data;
- //          this.next = null;
- //      }
- //  }
+ class Node {
+     constructor(data) {
+         this.data = data;
+         this.next = null;
+     }
+ }
 
- //  let node1 = new Node(1);
- //  let node2 = new Node(2);
- //  let node3 = new Node(3);
- //  let node4 = new Node(4);
- //  let node5 = new Node(5);
- //  let node6 = new Node(6);
+ let node1 = new Node(1);
+ let node2 = new Node(2);
+ let node3 = new Node(3);
+ let node4 = new Node(4);
+ let node5 = new Node(5);
+ let node6 = new Node(6);
 
- //  node1.next = node2;
- //  node2.next = node3;
- //  node3.next = node4;
- //  node4.next = node5;
- //  node5.next = node6;
+ node1.next = node2;
+ node2.next = node3;
+ node3.next = node4;
+ node4.next = node5;
+ node5.next = node6;
 
- //  function detectCycle(head) {
- //      let slow = head;
- //      let fast = head;
+ function detectCycle(head) {
+     let slow = head;
+     let fast = head;
 
- //      while (fast !== null && fast.next !== null) {
- //          slow = slow.next;
- //          fast = fast.next.next;
- //      }
- //      return slow;
- //  }
+     while (fast !== null && fast.next !== null) {
+         slow = slow.next;
+         fast = fast.next.next;
+     }
+     return slow;
+ }
 
- //  let meetingNode = detectCycle(node1);
- //  console.log(meetingNode.data);
-
+ let meetingNode = detectCycle(node1);
+ console.log(meetingNode.data);
 
 
 
