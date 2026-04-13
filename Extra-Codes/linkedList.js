53d8f482bb0a76bf2609f72caa2bcb4f27e29b2f
Muhammad Farjad Khan <farjadkhan823@gmail.com>
2026-04-13 11:51:23 +0500

    JS-DSA

---
+++ Extra-Codes/linkedList.js
@@ -0,0 +1,57 @@
+// 1 - singly linked list approach 
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+
+node1.next = node2;
+node2.next = node3;
+
+let current = node1;
+
+while (current !== null) {
+    console.log(current.data);
+    current = current.next;
+}
+
+
+
+
+
+// 2 - doubly linked list approach 
+
+// class Node {
+//     constructor(data) {
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
+
+// let node1 = new Node(1);
+// let node2 = new Node(2);
+// let node3 = new Node(3);
+// let node4 = new Node(4);
+// let node5 = new Node(5);
+
+// node1.next = node2;
+// node2.next = node3;
+// node3.next = node4;
+// node4.next = node5;
+// node5.next = node3;
+
+// let current = node1;
+// let count = 0;
+// let limit = 10;
+
+// while (current !== null && count < limit) {
+//     console.log(current.data);
+//     current = current.next;
+//     count++;
+// }
