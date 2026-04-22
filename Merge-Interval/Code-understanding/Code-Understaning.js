cdbfb964ec3c1c36b70e62b8b2427f3335e078a7
Muhammad Farjad Khan <farjadkhan823@gmail.com>
2026-04-22 12:05:58 +0500

    JS-DSA

--- Merge-Interval/Code-understanding/Code-Understaning.js
+++ Merge-Interval/Code-understanding/Code-Understaning.js
@@ -23,31 +23,56 @@
 
 // 1 function -> comparsion and swapping 
 
-let arr = [33, 54, 65, 12, 28, 67, 80, 11, 109];
+// let arr = [33, 54, 65, 12, 28, 67, 80, 11, 109];
 
-function heapify(arr, n, i) {
-    let largest = i;
-    let left = 2 * i + 1;
-    let right = 2 * i + 2;
+// function heapify(arr, n, i) {
+//     let largest = i;
+//     let left = 2 * i + 1;
+//     let right = 2 * i + 2;
 
-    if (left < n && arr[left] > arr[largest]) {
-        largest = left;
+//     if (left < n && arr[left] > arr[largest]) {
+//         largest = left;
+//     }
+//     if (right < n && arr[right] > arr[largest]) {
+//         largest = right;
+//     }
+//     if (largest !== i) {
+//         [arr[i], arr[largest]] = [arr[largest], arr[i]];
+//         heapify(arr, n, largest);
+//     }
+// }
+
+// function buildHeap(arr) {
+//     let n = arr.length;
+//     for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+//         heapify(arr, n, i);
+//     }
+// }
+// console.log("Original Array :", arr);
+// buildHeap(arr);
+// console.log("Max-Heap Array :", arr);
+
+// problem 1 -- ///
+
+function canAttendAll(intervals) {
+
+    intervals.sort((a, b) => a[0] - b[0]);
+
+
+    for (let i = 1; i < intervals.length; i++) {
+        let prev = intervals[i - 1];
+        let curr = intervals[i];
+
+        if (curr[0] < prev[1]) {
+            return false;
+        }
     }
-    if (right < n && arr[right] > arr[largest]) {
-        largest = right;
-    }
-    if (largest !== i) {
-        [arr[i], arr[largest]] = [arr[largest], arr[i]];
-        heapify(arr, n, largest);
-    }
+
+    return true;
 }
 
-function buildHeap(arr) {
-    let n = arr.length;
-    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
-        heapify(arr, n, i);
-    }
-}
-console.log("Original Array :", arr);
-buildHeap(arr);
-console.log("Max-Heap Array :", arr);
+console.log(canAttendAll([
+    [4, 5],
+    [5, 6],
+    [7, 9]
+]));
