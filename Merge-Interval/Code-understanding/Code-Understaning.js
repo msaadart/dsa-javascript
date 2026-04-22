142d68fe63482b8c15fbd248a3dcf20f46e43814
Muhammad Farjad Khan <farjadkhan823@gmail.com>
2026-04-22 13:07:49 +0500

    JS-DSA

--- Merge-Interval/Code-understanding/Code-Understaning.js
+++ Merge-Interval/Code-understanding/Code-Understaning.js
@@ -58,7 +58,6 @@ function canAttendAll(intervals) {
 
     intervals.sort((a, b) => a[0] - b[0]);
 
-
     for (let i = 1; i < intervals.length; i++) {
         let prev = intervals[i - 1];
         let curr = intervals[i];
