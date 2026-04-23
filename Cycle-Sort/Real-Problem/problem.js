ac71a9647985af6f3b692ed2eef04e8048f715f5
Muhammad Farjad Khan <farjadkhan823@gmail.com>
2026-04-23 12:08:29 +0500

    JS-DSA

--- Cycle-Sort/Real-Problem/problem.js
+++ Cycle-Sort/Real-Problem/problem.js
@@ -1,14 +1,32 @@
-let arr = [2, 4, 1, 3, 6];
-arr.sort((a, b) => a - b)
+// 1 --- find the missing numbers --- //
 
-function missingNumber(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== i + 1) {
-            return i + 1;
-        }
-    }
-    return arr.length + 1;
-}
+// let arr = [2, 3, 5, 8, 1, 4, 7];
+// arr.sort((a, b) => a - b);
 
-let output = missingNumber(arr);
-console.log(output);
+// function missingNumber(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] !== i + 1)
+//             return i + 1;
+//     }
+// }
+// let output = missingNumber(arr);
+// console.log(output);
+
+
+
+// 2 --- find the duplicate numbers --- //
+
+// let arr = [1, 2, 3, 2, 5, 4, 5];
+// arr.sort((a, b) => a - b);
+
+// function duplicate(arr) {
+//     let result = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === arr[i + 1]) {
+//             result.push(arr[i]);
+//         }
+//     }
+//     return result;
+// }
+// let output = duplicate(arr);
+// console.log(output);
