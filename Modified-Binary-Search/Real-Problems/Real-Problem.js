648ba152437c74fbddfd54fb3f7ff5b83b400eca
farjadkhan823-eng <farjadkhan823@gmail.com>
2026-05-14 16:53:15 +0500

    JS-DSA

--- Modified-Binary-Search/Real-Problems/Real-Problem.js
+++ Modified-Binary-Search/Real-Problems/Real-Problem.js
@@ -1,23 +1,48 @@
 
-function ceilSearch(arr, target) {
+//// Ceil of a number 
+
+// function ceilSearch(arr, target) {
+//     let left = 0;
+//     let right = arr.length - 1;
+//     let ans = -1;
+
+//     while(left <= right){
+//         let mid = Math.floor((left + right) / 2);
+
+//         if(arr[mid] === target){
+//             return arr[mid];   
+//         } 
+//         else if(arr[mid] > target){
+//             ans = arr[mid];
+//             right = mid - 1;
+//         }
+//         else{
+//             left = mid + 1;
+//         }
+//     } 
+//     return ans;
+// }
+// console.log(ceilSearch([2,4,6,11,8,13,15], 10));
+
+
+//// Bitonic Array Maximum
+
+function bitonicMax(arr) {
+
     let left = 0;
     let right = arr.length - 1;
-    let ans = -1;
 
-    while(left <= right){
+    while (left < right) {
         let mid = Math.floor((left + right) / 2);
 
-        if(arr[mid] === target){
-            return arr[mid];   
-        } 
-        else if(arr[mid] > target){
-            ans = arr[mid];
-            right = mid - 1;
-        }
-        else{
+        if (arr[mid] < arr[mid + 1]) {
             left = mid + 1;
         }
-    } 
-    return ans;
+        else {
+            right = mid;
+        }
+    }
+    return arr[left];
 }
-console.log(ceilSearch([2,4,6,11,8,13,15], 10));
+
+console.log(bitonicMax([1, 3, 8, 4, 2]));
