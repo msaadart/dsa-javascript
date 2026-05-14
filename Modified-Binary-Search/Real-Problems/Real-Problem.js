0d63aaf5382dfbcfb5d9ec3dc703456887ebb642
farjadkhan823-eng <farjadkhan823@gmail.com>
2026-05-14 15:45:24 +0500

    JS-DSA

--- Modified-Binary-Search/Real-Problems/Real-Problem.js
+++ Modified-Binary-Search/Real-Problems/Real-Problem.js
@@ -1,25 +1,23 @@
 
-
-function ceiling(arr, target) {
+function ceilSearch(arr, target) {
     let left = 0;
     let right = arr.length - 1;
     let ans = -1;
 
-    while (left <= right) {
+    while(left <= right){
         let mid = Math.floor((left + right) / 2);
 
-        if (arr[mid] === target) {
-            return arr[mid];
-        }
-        else if (arr[mid] > target) {
+        if(arr[mid] === target){
+            return arr[mid];   
+        } 
+        else if(arr[mid] > target){
             ans = arr[mid];
             right = mid - 1;
         }
-        else {
+        else{
             left = mid + 1;
         }
-    }
+    } 
     return ans;
 }
-
-console.log(ceiling([1, 2, 4, 6, 8, 10], 7));
+console.log(ceilSearch([2,4,6,11,8,13,15], 10));
