f5988ae60c70938d64c481f8fb6e26cee51c5e4a
farjadkhan823-eng <farjadkhan823@gmail.com>
2026-05-14 10:38:22 +0500

    JS-DSA

--- Breadth-First-Search/Code-Understanding/bfs.js
+++ Breadth-First-Search/Code-Understanding/bfs.js
@@ -11,6 +11,11 @@ class TreeNode {
 let root = new TreeNode(1);
 root.left = new TreeNode(2);
 root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+root.right.left = new TreeNode(6);
+root.right.right = new TreeNode(7);
+
 
 let queue = [root];
 while (queue.length > 0) {
---
+++ Modified-Binary-Search/Code-Understanding/modified.js
@@ -0,0 +1,22 @@
+
+function modified(arr, target) {  // 1 2 3 4 5 6 7 8 9
+    arr.sort((a,b) => a-b);       
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1
+        } else {
+            right = mid - 1;
+        }
+
+    }
+    let err = "not found"
+    return err;
+}
+console.log(modified([1, 7, 3, 9, 5, 6,2, 8, 4], 7));
