7b8335d51b492c23a13b5b38adaf410b8e7430dd
farjadkhan823-eng <farjadkhan823@gmail.com>
2026-05-22 15:29:45 +0500

    JS-DSA

--- K-Way-Merge/Real-Problems/problem.js
+++ K-Way-Merge/Real-Problems/problem.js
@@ -1,30 +1,70 @@
-function kWayMerge(arrays, k){
+// function kWayMerge(arrays, k){
 
-    let result = [];
+//     let result = [];
+//     let heap = [];
+
+//     for(let i = 0; i < arrays.length; i++){
+//         heap.push([arrays[i][0], i, 0]);
+//     }
+//     heap.sort((a,b) => a[0] - b[0]);
+
+//     while(heap.length > 0){
+//         let [value, arrayIndex, elementIndex] = heap.shift();
+//         result.push(value);
+
+//         let nextIndex = elementIndex + 1;
+
+//         if(nextIndex < arrays[arrayIndex].length){
+//             let nextValue = arrays[arrayIndex][nextIndex];
+//             heap.push([nextValue, arrayIndex, nextIndex]);
+//             heap.sort((a,b)=>a[0]-b[0]);
+//         }
+//     }
+//     return result.slice(0, k);
+// }
+// let arrays = [
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [3, 6, 9]
+// ];
+// console.log(kWayMerge(arrays, 5));
+
+
+
+
+function kWayMerge(arrays, k) {
+
+    // let result = [];
     let heap = [];
 
-    for(let i = 0; i < arrays.length; i++){
+    for (let i = 0; i < arrays.length; i++) {
         heap.push([arrays[i][0], i, 0]);
     }
-    heap.sort((a,b) => a[0] - b[0]);
+    heap.sort((a, b) => a[0] - b[0]);
+  
+    let count = 0;
 
-    while(heap.length > 0){
+    while (heap.length > 0) {
         let [value, arrayIndex, elementIndex] = heap.shift();
-        result.push(value);
+
+        count++;
+        if(count === k){
+            return value;
+        }
 
         let nextIndex = elementIndex + 1;
 
-        if(nextIndex < arrays[arrayIndex].length){
+        if (nextIndex < arrays[arrayIndex].length) {
             let nextValue = arrays[arrayIndex][nextIndex];
             heap.push([nextValue, arrayIndex, nextIndex]);
-            heap.sort((a,b)=>a[0]-b[0]);
+            heap.sort((a, b) => a[0] - b[0]);
         }
     }
-    return result.slice(0, k);
+    // return result[k - 1];
 }
 let arrays = [
     [1, 4, 7],
     [2, 5, 8],
     [3, 6, 9]
 ];
-console.log(kWayMerge(arrays, 5));
+console.log(kWayMerge(arrays, 3));
