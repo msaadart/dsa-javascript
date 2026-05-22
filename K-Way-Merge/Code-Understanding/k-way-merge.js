80f731edb671a3a89328b4f79ae759009ba03343
farjadkhan823-eng <farjadkhan823@gmail.com>
2026-05-22 13:02:46 +0500

    JS-DSA

--- K-Way-Merge/Code-Understanding/k-way-merge.js
+++ K-Way-Merge/Code-Understanding/k-way-merge.js
@@ -18,3 +18,37 @@ for (let i = 0; i < matrix.length; i++) {
     }
 }
 console.log(maxNumber);
+
+
+
+
+function kWayMerge(arrays){
+
+    let result = [];
+    let heap = [];
+
+    for(let i = 0; i < arrays.length; i++){
+        heap.push([arrays[i][0], i, 0]);
+    }
+    heap.sort((a,b) => a[0] - b[0]);
+
+    while(heap.length > 0){
+        let [value, arrayIndex, elementIndex] = heap.shift();
+        result.push(value);
+
+        let nextIndex = elementIndex + 1;
+
+        if(nextIndex < arrays[arrayIndex].length){
+            let nextValue = arrays[arrayIndex][nextIndex];
+            heap.push([nextValue, arrayIndex, nextIndex]);
+            heap.sort((a,b)=>a[0]-b[0]);
+        }
+    }
+    return result;
+}
+let arrays = [
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
+];
+console.log(kWayMerge(arrays));
