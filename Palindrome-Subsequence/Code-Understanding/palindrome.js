88a48b4867cbceea51c8c2a77f03e2d8fc4273ee
farjadkhan823-eng <farjadkhan823@gmail.com>
2026-06-02 14:19:35 +0500

    JS-DSA

---
+++ Palindrome-Subsequence/Code-Understanding/palindrome.js
@@ -0,0 +1,25 @@
+function longestPalindromeSubseq(s) {
+    let n = s.length;
+    let dp = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) dp[i][i] = 1;
+
+    for (let i = n - 1; i >= 0; i--) {
+        for (let j = i + 1; j < n; j++) {
+            if (s[i] === s[j]) {
+                dp[i][j] = dp[i + 1][j - 1] + 2;
+            } else {
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[0][n - 1];
+    // return n - dp[0][n - 1]; // for minimum deletions to make the string a palindrome
+}
+
+let str = "ABXCBAY";
+console.log("Longest Palindrome Size:", longestPalindromeSubseq(str));
+
+
+
+
